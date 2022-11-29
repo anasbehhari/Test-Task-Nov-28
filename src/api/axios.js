bbf92bd2f14d5971fc5f37d6a8b0d120d1f12b26
Anas <an.behhari@gmail.com>
2022-11-29 22:52:52 +0100

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL : "https://test-task-nov-28-api.vercel.app/" || import.meta.env.VITE_APP_BASE_URL
+    baseURL : import.meta.env.VITE_APP_BASE_URL
 })
