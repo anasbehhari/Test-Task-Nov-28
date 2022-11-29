287e5e2c4ae0de51341373f7c558eb434b74d287
Anas <an.behhari@gmail.com>
2022-11-29 18:13:00 +0100

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,37 +1,85 @@
-import "./form.css"
+import { useState } from "react";
+import axios from "../api/axios";
+import "./form.css";
 import Input from "./form/Input";
 import Select from "./form/Select";
 
 const Form = () => {
-  let options = [
-    {
-      id: 1,
-      value: 1,
-      name: "Manufacturing",
-    },
-    {
-      id: 2,
-      value: 2,
-      name: "Construction materials",
-    },
-
-    {
-      id: 3,
-      value: 3,
-      name: "Electronics and Optics",
-    }
-  ];
+  var [Data, setData] = useState();
   const handlesubmit = (e) => {
     e.preventDefault();
-    // let values = new FormData(e.target);
-    // let data  = Object.fromEntries(values.entries())
-    // console.log(data);
+    let values = new FormData(e.target);
+    let data = Object.fromEntries(values.entries());
+    if (data.name && data.terms == "on") {
+      let selected = e.target[1].selectedOptions;
+      let selectedSectors = [];
+      for (let i = 0; i < selected.length; i++) {
+        const el = selected[i];
+        selectedSectors.push({
+          id: el.getAttribute("data-id"),
+          name: el.textContent,
+        });
+      }
+      if (selectedSectors.length > 0) {
+        data.sectors = selectedSectors;
+        //Store the state
+        setData(data);
+
+        //store to localstorage
+        localStorage.setItem("data", JSON.stringify(data));
+        localStorage.setItem("Logged", true);
+
+        //success push to database
+        //request to API for storing data after Rechecking all data.
+        const options = {
+          url: "/api/user/add",
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+          },
+          data,
+        };
+        axios(options)
+        .then(response => {
+          if(response.status==200) {
+            //redirect to next component 
+          }
+        });
+      } else {
+        //someone played with the HTML element fire a error and load the page
+        console.log("jm3 krk1");
+      }
+    } else {
+      //someone played with the HTML element fire a error and load the page
+      console.log("jm3 krk2");
+
+    }
   };
+
   return (
     <form onSubmit={handlesubmit}>
-      <Input  className="text"label="1. username" name="name" type="text" placeholder="Enter a valid username." error="*field required" />
-      <Select label="2. Sectors" name="sectors" placeholder="pick the Sectors you are currently involved in." multiple={true} options={options} error="*field required"/>
-      <Input className="checkbox" label="Agree to terms" name="terms" type="checkbox" error="*"/>
+      <Input
+        className="text"
+        label="1. username"
+        name="name"
+        type="text"
+        placeholder="Enter a valid username."
+        error="*field required"
+      />
+      <Select
+        label="2. Sectors"
+        name="sectors"
+        placeholder="pick the Sectors you are currently involved in."
+        error="*field required"
+      />
+      <Input
+        className="checkbox"
+        label="Agree to terms"
+        name="terms"
+        type="checkbox"
+        error="*"
+      />
       <button>Send</button>
     </form>
   );
--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -1,24 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "../../api/axios";
 import "./select.css";
 const Select = (props) => {
+  //init state for focus event for better UI
   const [focused, setFocused] = useState(false);
-
+  //Focus function
   const handlefocus = (e) => {
     setFocused(true);
   };
+
+
+  //init state for loading component
+  const [loading, setLoading] = useState(true);
+  //init state for storing options and retreiving it from database
+  const [options, setOptions] = useState(true);
+
+  //Request function
+  const getOptions = async () => {
+    const options = {
+      method: "GET",
+      url: "/api/sectors",
+      headers: {
+        "x-rapidapi-key": "",
+        "x-rapidapi-host": "",
+      },
+    };
+    try {
+      const { data } = await axios.request(options);
+      setOptions(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  //firing function for getting data from database 
+  useEffect(() => {
+    getOptions();
+  }, []);
+
+  //check if data is loaded 
+  if (loading) return <p> Loading ... </p>;
+
   return (
     <div className="SelectForm">
       <label> {props.label}</label>
       <span>{props.placeholder}</span>
       <select
         name={props.name}
-        multiple={props.multiple}
+        multiple={true}
         required={true}
         onBlur={handlefocus}
         focused={focused.toString()}
       >
-        {props.options.map((option) => (
-          <option key={option.id} value={option.value}>
+        {options.map((option) => (
+          <option data-id={option._id} key={option._id} value={option.name}>
             {option.name}{" "}
           </option>
         ))}
